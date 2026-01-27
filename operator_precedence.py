2df5dac5cd283535473d205c83d73fc564636364
charita0427 <charitamadhi@gmail.com>
2026-01-27 16:43:25 -0500

    Add files via upload

---
+++ operator_precedence.py
@@ -0,0 +1,15 @@
+v= 4
+w= 5
+x= 8
+y= 2
+z= 0
+z = (v+w) * x/y
+print("Value of (v+w) * x/ y is", z)
+
+name = "Alexa"
+age= 0
+
+if name == "Alexa" or name == "John" and age >= 2:
+    print("Hello! Welcome.")
+else:
+    print("Good bye!!")
