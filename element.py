91a52d17cf270fa84d5da34a0a73a70ad06c7c9b
charita0427 <charitamadhi@gmail.com>
2026-07-28 17:02:25 -0400

    Add files via upload

---
+++ element.py
@@ -0,0 +1,13 @@
+empty_list = [ ]
+print()
+
+number = [1,2,3,4,5]
+print (number)
+
+triples = [1,2,3] * 3
+print (triples)
+
+aList = [100,200,300,400,500]
+aList = aList[::-1]
+#first : = start the code, second : = stop the code, -1 = go in reverse order
+print (aList, "\n")
