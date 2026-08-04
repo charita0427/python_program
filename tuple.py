78c2d50f8dd7b3b5f7e10ec45ef28cbc1691da75
charita0427 <charitamadhi@gmail.com>
2026-08-04 17:09:19 -0400

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,19 @@
+tuplex = ('tuple', False, 5.6, 4)
+print(tuplex)
+
+tuplex = (3,4,6,7,2,4,6)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (50, 60, 80, 90, 80)
+print (tuple1.count(80)) #first need to write down the name and then count
+
+tuplex = (1,5,6,7,4,8,9,3,2,9)
+_slice = tuplex [4:5] #it starts at 4 and stops at 5
+
+print (_slice)
+
+_slice = tuplex [:6] #it only picks 6 nubers from the string 
+print (_slice)
